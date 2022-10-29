0eef1a4a8aa0c7ff97b9520b55caae53e646e8e9
ANASTASIIAZAIARNIUK <113503637+ANASTASIIAZAIARNIUK@users.noreply.github.com>
2022-10-29 20:32:13 +0200

    Assignment 3

--- sketch.js
+++ sketch.js
@@ -1,16 +1,62 @@
+let pg
+let img
+let GTfont
+let txt = "HOME"
+let mySound
+
 function preload() {
-	// put preload code here
+  GTfont = loadFont ('GT-Haptik-Bold.OTF');
+  img = loadImage ('home2.jpg'); 
+  pg = createGraphics(586, 810); 
+  mySound = loadSound("siren.mp3");
+}
+ 
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background("black");
 }
 
-function setup() {
-	createCanvas(windowWidth, windowHeight);
-	// put setup code here
-	const message = "This is a template reposotory\nfor the course elective Creative Coding\nCommunication Design, Politecnico di Milano";
-	textAlign(CENTER, CENTER);
-  textSize(16)
-	text(message, width/2, height/2);
+function mousePressed() {
+  if (mySound.isPlaying() ) { 
+    mySound.stop();
+    background("black");
+  } else {
+    mySound.play();
+    background("red");
+  } 
 }
 
 function draw() {
-	// put drawing code here
+
+  pg.push();
+  pg.image(img, 0, 0);
+  pg.pop();
+
+  pg.push();
+  imageMode(CENTER);
+  image(pg, width/2, height/2);
+  pg.pop();
+
+  fill("white");
+  smooth();
+  noStroke();
+  textFont(GTfont);
+  textSize(190);
+  textAlign(CENTER);
+
+  pg.push();
+  let txtW = textWidth(txt);
+  let spacing = txtW / txt.length;
+  pg.pop();
+
+  for(let i = 0; i < txt.length; i++){
+    let c = txt.charAt(i);
+    let offsetX = random(-spacing / 180, spacing / 180);
+    let offsetY = random(-spacing / 180, spacing / 180);
+    let startX = (width - txtW) / 2 + spacing / 2;
+    let y = height / 2;
+    text(c, startX + i * spacing + offsetX, y + offsetY);
+
+  }
 }
+
